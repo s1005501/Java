5ce5e65a632144c84b29df9ff78171e033eeff9e
Kevin Gan <s1005501@gmail.com>
2023-12-12 17:23:42 +0800

    12/12下班前java進度

---
+++ src/chapter0/Double.java
@@ -0,0 +1,11 @@
+package chapter0;
+
+public class Double {
+    public static void main(String[] args) {
+        // double表示值必須是小數點
+        
+        final double pi = 3.14;
+        // pi前已經加上final就不可再做更改了，會噴錯
+        // pi = 666;
+    }
+}
---
+++ src/chapter0/Main.java
@@ -0,0 +1,12 @@
+package chapter0;
+public class Main {
+    // 輸入jsvm可快速生成public static void main(String[] args){}
+    public static void main(String[] args) {
+        // 輸入sout可快速生成System.out.println()
+        System.out.println("Hello World");
+
+        // 輸出的資料也可以是簡單的表達式
+        System.out.println(4 + 6);
+        System.out.println(4.0 / 3);
+    }
+}
