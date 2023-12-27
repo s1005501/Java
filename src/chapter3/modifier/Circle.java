653fb4c0797afa5f11f35a32ef434eeb48247ef6
KevinGan <s1005501@gmail.com>
2023-12-28 02:32:17 +0800

    12/28/2:32

--- src/chapter3/modifier/Circle.java
+++ src/chapter3/modifier/Circle.java
@@ -1,13 +1,20 @@
 package chapter3.modifier;
 
 // class的修飾符
-// class的修飾符不能用private
 // 訪問
-// public，代表這個class可被其他class訪問
+// 代表這個class可被其他class訪問，ex:public、private
 
 // 非訪問
 // abstract，代表這個class是一個受限的class，不能用來創建object，他主要的功能是讓其他class來繼承他
-// 
+// 使用時直接加在class前即可
+// public abstract class class名稱{}
 public class Circle {
 
+    // 實務上變數大多都會設成是private，method則都會設成public
+    // 但仍然會有例外，例如會把method設成private的情況是該method只在那個class裡使用
+    private double radius = 1.0;
+
+    public double getRadius() {
+        return this.radius;
+    }
 }
--- src/chapter3/modifier/Modifier.java
+++ src/chapter3/modifier/Modifier.java
@@ -2,11 +2,23 @@ package chapter3.modifier;
 
 // 修飾符(modifier)
 // 功能是提供class、變數額外的訊息
-// 分為訪問及非訪問修飾符。訪問修飾符可以控制訪問的級別，即public、private。而非訪問修飾符不控制訪問級別，而是提供其他功能
+// 分為訪問及非訪問修飾符。訪問修飾符可以控制訪問的級別，例如public、private。而非訪問修飾符則是提供其他功能，例如abstract、final
 // 如果在class宣告時沒有給public、private，java就會幫你套預設，但建議還是請記得都加上
+// 在一個class、變數上可以同時加上多個修飾符
+
+// 可在所有method、變數都可以使用的修飾符
+// public(訪問):讓code在任何地方都能被使用
+// private(訪問):讓code只能在自己的class中使用，從這個class新建立起來的objects也是取不到private的資料的
+// protected(訪問):讓code可以在同一個package和subclass中使用
+// final(非訪問):讓methods、變數不能被覆蓋/修改。
+// static(非訪問):讓methods、變數屬於class本身，而不是從這個class新建立起來的objects
 
 public class Modifier {
     public static void main(String[] args) {
+        Circle c = new Circle();
 
+        // 取不到private的資料
+        // System.out.println(c.radius);
+        System.out.println(c.getRadius());
     }
 }
